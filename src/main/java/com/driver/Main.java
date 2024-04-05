88e509b7f84bb52207478a0e14ecdcad1f61f28f
cipher <sarthakshirbhate081@gmail.com>
2024-04-06 01:03:44 +0530

    commit one

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // Try setting a value to name directly (will result in error)
+        // obj.name = "John"; // This will cause an error because name is private
+
+        // Try setting a value to name using setter method
+        obj.setName("John");
+
+        // Try printing the value of name directly (will result in error)
+        // System.out.println(obj.name); // This will cause an error because name is private
+
+        // Print the value of name using getter method
+        System.out.println(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Getter method
+    public String getName() {
+        return name;
+    }
+
+    // Setter method
+    public void setName(String name) {
+        this.name = name;
+    }
 }
